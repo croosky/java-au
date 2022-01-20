44dd89f4f2a543e71b053f62394c555cdfb9a54f
croosky <khaninamilana@gmail.com>
2022-01-20 19:13:55 +0300

    generator

--- generator/src/MDEntity.java
+++ generator/src/MDEntity.java
@@ -4,9 +4,18 @@ public class MDEntity implements ItemEntity{
     private String title;
     private String url;
     private String solution;
-    public static MDEntity parseEntity(List<String> solutionFile) {
-        String[] parts = solutionFile.split(System.lineSeparator());
-        String title = parts[0];
 
+    MDEntity(String title, String url, String solution){
+        this.title = title;
+        this.url = url;
+        this.solution = solution;
+    }
+
+    public static MDEntity parseEntity(List<String> s) {
+        if(s.size() < 4) {
+            return new MDEntity(null,null,null);
+        }
+        String joined = String.join("\n", s);
+        return new MDEntity(s.get(0), s.get(1), "```java" + "\n" + joined + "```");
     }
 }
--- generator/src/SolutionFile.java
+++ generator/src/SolutionFile.java
@@ -1,7 +1,23 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SolutionFile {
+    private String fileName;
+    private FileType fileType;
+    private List<ItemEntity> content;
+
+    public SolutionFile(String fileName, List<ItemEntity> content, FileType fileType) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.content = content;
+    }
     public static SolutionFile parse(String fileName, List<String> oldContent, FileType fileType) {
+    }
+
+    private String mdToString(){
+        String title = this.content.stream().map(ItemEntity::getTitle).collect(Collectors.joining("\n"));
+        String solutions = this.content.stream().map(ItemEntity::getForm).collect(Collectors.joining("\n\n"));
+        return "# " + this.fileName + "\n\n" + title + "\n\n" + solutions;
     }
 
     public Object add(Object parseEntity) {
